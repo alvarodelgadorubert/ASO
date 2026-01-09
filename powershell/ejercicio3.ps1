534d71b3558bb4212d3cb7dfcf9de24c4b359ea7
Alvaro Delgado <148425576+alvarodelgadorubert@users.noreply.github.com>
2026-01-09 16:29:52 +0100

    Añadimos el ejercicio3

---
+++ powershell/ejercicio3.ps1
@@ -0,0 +1,6 @@
+﻿Clear-Host
+[int]$horas = Read-Host "Escribe tu numero de horas trabajadas"
+[int]$coste = Read-Host "Escribe el coste por hora trabajada"
+$salario = $horas * $coste
+
+Write-Host "Tu salario es de" $salario "euros"
