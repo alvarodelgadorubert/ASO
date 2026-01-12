7385910c4d200cac1b218839d6c049d9d34a603c
Alvaro Delgado <148425576+alvarodelgadorubert@users.noreply.github.com>
2026-01-12 09:37:03 +0100

    Delete powershell/ejercicio1.ps1

--- powershell/ejercicio1.ps1
+++
@@ -1,9 +0,0 @@
-﻿Clear-Host
-$username = $env:USERNAME
-Write-Host "Hola $username"
-$directorio = $env:USERPROFILE
-Write-Host "Tu directorio de trabajo es $directorio"
-$nombre_dominio = $env:USERDOMAIN
-Write-Host "Perteneces al dominio $nombre_dominio"
-$nombre_equipo = $env:COMPUTERNAME
-Write-Host "Tu equipo se llama $nombre_equipo"
