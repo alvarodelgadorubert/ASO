07056f69209e8f0fc2173395bb773f8d15f5faa1
Alvaro Delgado <148425576+alvarodelgadorubert@users.noreply.github.com>
2026-01-12 09:37:23 +0100

    Delete powershell/ejercicio3.ps1

--- powershell/ejercicio3.ps1
+++
@@ -1,6 +0,0 @@
-﻿Clear-Host
-[int]$horas = Read-Host "Escribe tu numero de horas trabajadas"
-[int]$coste = Read-Host "Escribe el coste por hora trabajada"
-$salario = $horas * $coste
-
-Write-Host "Tu salario es de" $salario "euros"
