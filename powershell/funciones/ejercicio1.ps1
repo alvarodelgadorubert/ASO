c8d74712477c18527b15f3ebff0695b696a94d6c
Alvaro Delgado Rubert <148425576+alvarodelgadorubert@users.noreply.github.com>
2026-01-19 08:35:27 +0100

    subo los ejercicios de funciones

---
+++ powershell/funciones/ejercicio1.ps1
@@ -0,0 +1,14 @@
+﻿function Calcular-IVA {
+    param (
+        [double]$precio,
+        [double]$iva = 21
+    )
+    
+    $total = $precio + ($precio * ($iva / 100))
+    return $total
+}
+
+$monto = Read-Host "Introduce el precio sin IVA"
+$resultado = Calcular-IVA -precio $monto
+
+Write-Host "El precio final con el 21% de IVA es: $resultado €"
---
+++ powershell/funciones/ejercicio2.ps1
@@ -0,0 +1,21 @@
+﻿function Crear-Usuario {
+    param (
+        $nombre,
+        $apellido,
+        $departamento
+    )
+    Write-Host ">> Creando usuario: $nombre $apellido en el departamento de $departamento..."
+}
+
+$rutaCsv = "usuarios.csv"
+
+if (Test-Path $rutaCsv) {
+    $listaUsuarios = Import-Csv -Path $rutaCsv -Delimiter ","
+
+    foreach ($user in $listaUsuarios) {
+        Crear-Usuario -nombre $user.nombre -apellido $user.apellido -departamento $user.departamento
+    }
+    Write-Host "`nProceso finalizado correctamente."
+} else {
+    Write-Host "Error: No se encuentra el archivo $rutaCsv"
+}
