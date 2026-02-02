dae59f00186bdf0e6a6abe44f95252bf70543d39
Alvaro Delgado Rubert <148425576+alvarodelgadorubert@users.noreply.github.com>
2026-02-02 08:33:58 +0100

    Add files via upload

---
+++ powershell/active directory/p4/perfiles_moviles.ps1
@@ -0,0 +1,10 @@
+﻿$personalOU = "OU=PERSONAL,OU=Empresa,DC=empresa,DC=local"
+
+$usuariosPersonal = Get-ADUser -Filter * -SearchBase $personalOU
+
+foreach ($u in $usuariosPersonal) {
+    $samAccount = $u.SamAccountName
+    $profilePath = "\\Empresa-dc1\Empresa_users$\" + $samAccount
+    
+    Set-ADUser -Identity $samAccount -ProfilePath $profilePath
+}
